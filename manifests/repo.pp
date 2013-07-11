f4f95a146fc83660fd88689fd73d79c6f797e732
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2013-07-11 08:51:40 -0500

    added support for ubuntu

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,8 +1,11 @@
 class cloudmonitoring::repo {
+
+  $package_url = "http://stable.packages.cloudmonitoring.rackspace.com"
+  $signing_url = "https://monitoring.api.rackspacecloud.com/pki/agent"
+  $signingkey = 'linux.asc'
+  
   if $::osfamily == 'RedHat' {
-    if $::os_maj_version >= 6 {
-      $signingkey = 'linux.asc'
-    } else { 
+    if $::os_maj_version < 6 { 
       $signingkey = $operatingsystem ? {
         'RedHat' => 'redhat-5.asc',
         'CentOS' => 'centos-5.asc',
@@ -11,9 +14,20 @@ class cloudmonitoring::repo {
         
     yumrepo { 'rackspace_monitoring':
       descr   => 'Rackspace Monitoring',
-      baseurl => "http://stable.packages.cloudmonitoring.rackspace.com/${::os_lower}-${::os_maj_version}-${::architecture}",
-      gpgkey  => "https://monitoring.api.rackspacecloud.com/pki/agent/${signingkey}",
+      baseurl => "${package_url}/${::os_lower}-${::os_maj_version}-${::architecture}",
+      gpgkey  => "${signing_url}/${signingkey}",
       enabled => 1,
+    }
+  }
+
+  if $::osfamily == 'Debian' {
+    include apt
+    apt::source { "rackspace_monitoring":
+      location    => "${package_url}/${::os_lower}-${::operatingsystemrelease}-${::hardwaremodel}",
+      release     => "cloudmonitoring",
+      include_src => false,
+      key         => "D05AB914",
+      key_source  => "${signing_url}/${signingkey}",
     }
   }
 }
